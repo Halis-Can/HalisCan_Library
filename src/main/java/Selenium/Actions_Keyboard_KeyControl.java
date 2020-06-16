0f63e3619fcefc20ace42f8b524c753d3d42790d
Halis-Can <haliscan@hotmail.com>
2020-06-16 15:57:04 -0400

    deneme

--- src/main/java/Selenium/Actions_Keyboard_KeyControl.java
+++ src/main/java/Selenium/Actions_Keyboard_KeyControl.java
@@ -24,7 +24,7 @@ public class Actions_Keyboard_KeyControl {
         driver.get("http://www.uitestpractice.com/Students/Actions");
 
     }
-    //first commit 45
+    //first commit 11111111
 
     @Test
     public void KeyControl1(){
